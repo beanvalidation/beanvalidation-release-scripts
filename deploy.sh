1c4690f8d316b29ad866023bc6e1fb95fbcb3e90
Guillaume Smet <guillaume.smet@gmail.com>
2019-10-16 16:56:03 +0200

    layout is supposed to have been removed but apparently not...

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ if [ -z "$PROJECT" ]; then
        exit 1
 fi
 
-mvn -Prelease,documentation-pdf clean deploy -s $HOME/.m2/settings-search-release.xml -DskipTests=true -Dcheckstyle.skip=true -DperformRelease=true -Dmaven.compiler.useIncrementalCompilation=false -DaltReleaseDeploymentRepository="jboss-releases-repository::https://repository.jboss.org/nexus/service/local/staging/deploy/maven2/"
+mvn -Prelease,documentation-pdf clean deploy -s $HOME/.m2/settings-search-release.xml -DskipTests=true -Dcheckstyle.skip=true -DperformRelease=true -Dmaven.compiler.useIncrementalCompilation=false -DaltReleaseDeploymentRepository="jboss-releases-repository::default::https://repository.jboss.org/nexus/service/local/staging/deploy/maven2/"
 
 popd
